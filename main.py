33fa9ead7b97640fcfea2c78646b74aa0428e99a
Diwas Rai <diwasrai38@yahoo.com>
2025-10-27 19:12:15 +0000

    feat: aggregate player stats for chosen columns

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pandas as pd
+from pandas.api.types import is_numeric_dtype
 import os
 from dotenv import load_dotenv
+from stats import stats_to_aggregate
 
 load_dotenv()
 
@@ -11,3 +13,9 @@ df = pd.read_csv(PATH_TO_DATA_FILE)
 
 print("Filtering for major league games...")
 df = df[df["league"].isin(["LEC", "LCK", "LPL", "LCS", "WRLDS", "MSI"])]
+
+print("Aggregating pro stats...")
+numeric_cols = [col for col in stats_to_aggregate if col in df.columns and is_numeric_dtype(df[col])]
+player_agg_stats = df.groupby('playername')[numeric_cols].mean()
+
+print(player_agg_stats)
