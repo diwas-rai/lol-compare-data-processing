b2371234b40de4e37331201d3f4f14d15dc37669
Diwas Rai <diwasrai38@yahoo.com>
2025-10-28 13:09:50 +0100

    style: fix styling with black and isort

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import pandas as pd
-from pandas.api.types import is_numeric_dtype
 import os
+
+import pandas as pd
 from dotenv import load_dotenv
+from pandas.api.types import is_numeric_dtype
+
 from stats import stats_to_aggregate
 
 load_dotenv()
@@ -12,13 +14,15 @@ print("Loading data...")
 df = pd.read_csv(PATH_TO_DATA_FILE)
 
 print("Filtering for major league games...")
-#TODO: figure out how to work with LPL stats
+# TODO: figure out how to work with LPL stats
 df = df[df["league"].isin(["LEC", "LCK", "LCS", "WRLDS", "MSI"])]
 
 print("Aggregating pro stats...")
-numeric_cols = [col for col in stats_to_aggregate if col in df.columns and is_numeric_dtype(df[col])]
-player_agg_stats = df.groupby('playername')[numeric_cols].mean()
-player_agg_stats["games_played"] = df.groupby('playername').size()
+numeric_cols = [
+    col for col in stats_to_aggregate if col in df.columns and is_numeric_dtype(df[col])
+]
+player_agg_stats = df.groupby("playername")[numeric_cols].mean()
+player_agg_stats["games_played"] = df.groupby("playername").size()
 
 player_agg_stats = player_agg_stats[player_agg_stats["games_played"] > 30]
 print(player_agg_stats)
